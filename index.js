687abf4d0db009f01aee4050b5dce8f1750e9e5d
Renato Alves <devrenatoalves@gmail.com>
2024-11-13 20:19:58 -0300

    Hello, World!

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const mysql = require ("mysql");
 const express = require ("express");
-const hbrs = require ("express-handlebars");
+const exphbs = require ("express-handlebars");
+
 const app = express();
+
+app.use(express.static("public"));
 
 app.listen(3000)
 
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+
+  res.render("home")
+});
