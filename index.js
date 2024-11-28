1a9ae5e05ef51f230b370ee85f04e17457d31647
Renato Alves <devrenatoalves@gmail.com>
2024-11-27 22:19:25 -0300

    CONNECTION DATABASE PROJETT AND SQL LOGIN PAGE

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const mysql = require ("mysql");
 const express = require ("express");
 const exphbs = require ("express-handlebars");
-
+const pool = require ("./db/conn");
 const app = express();
 
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+app.use(express.json());
+
 app.use(express.static("public"));
-
-app.listen(3000)
-
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+///////////////////////////////////////////////////////////////////TELA DE LOGIN
+
+
 app.get("/", (req, res) => {
 
-  res.render("home")
+  const user = req.body.user;
+  const password = req.body.password;
+
+  const sql = `INSERT INTO users  (usersname, password) VALUES (?, ?)`
+  const data = [username, password];
+
+  pool.query(sql, data, function (data, err) {
+    if(err) {
+      console.log("err")
+    };
+  });
+
+  res.render("login")
 });
+
+///////////////////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////DEFININDO PORTA
+app.listen(3000);
