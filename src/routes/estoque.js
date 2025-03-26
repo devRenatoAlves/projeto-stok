7ee8cde7bf71f42f8e3ce5c8de7e66c4078f6379
Renato Alves <devrenatoalves@gmail.com>
2025-03-26 08:45:49 -0300

    ADD username h1 stok page

--- src/routes/estoque.js
+++ src/routes/estoque.js
@@ -4,8 +4,8 @@ const authMiddleware = require('../public/js/authMiddleware.js');
 
 // Rota protegida
 router.get("/", authMiddleware, (req, res) => {
-  console.log(`Usuário logado: ${req.user.id}`); // Debug opcional
-  res.render("estoque", { userId: req.user.id });
+  console.log(`Usuário logado: ${req.user.username}`); // Debug opcional
+  res.render("estoque", { username: req.user.username });
 });
 
 module.exports = router;
